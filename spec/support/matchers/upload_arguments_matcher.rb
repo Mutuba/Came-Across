7295602a85725d5b469f509e188256d417af5d9a
Mutuba <danielmutubait@gmail.com>
2023-07-31 15:43:15 +0300

    clean up the files

--- spec/support/matchers/upload_arguments_matcher.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# spec/support/matchers/upload_arguments_matcher.rb
-RSpec::Matchers.define :upload_arguments do |expected_arguments|
-  match do |actual_arguments|
-    expect(actual_arguments.keys).to match_array(expected_arguments.keys)
-    expected_arguments.all? { |key, value| expect(actual_arguments[key]).to eq(value) }
-  end
-end
